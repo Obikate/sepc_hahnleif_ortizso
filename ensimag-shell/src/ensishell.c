0bf7f6d2c929e40862a85d8d0f40bd47c5a412a4
Felix <felix.hahnlein@ensimag.grenoble-inp.fr>
2016-03-28 19:38:28 +0200

    ensishell.c

--- ensimag-shell/src/ensishell.c
+++ ensimag-shell/src/ensishell.c
@@ -7,6 +7,10 @@
 
 #include <stdio.h>
 #include <stdlib.h>
+#include <fcntl.h>
+#include <unistd.h>
+#include <sys/types.h>
+#include <sys/stat.h>
 
 #include "variante.h"
 #include "readcmd.h"
@@ -26,8 +30,8 @@
 
 
 typedef struct bg_children{
-  pid_t pid;
-  struct bg_children * next;
+    pid_t pid;
+    struct bg_children * next;
 
 } bg_children_t;
 
@@ -35,179 +39,231 @@ bg_children_t *liste_children_bg;
 
 
 void add_bg(bg_children_t ** head, pid_t pid_a_ajouter){
-  bg_children_t * new_bg_children;
-  new_bg_children=malloc(sizeof(bg_children_t));
+    bg_children_t * new_bg_children;
+    new_bg_children=malloc(sizeof(bg_children_t));
 
-  new_bg_children->pid=pid_a_ajouter;
-  new_bg_children->next=*head;
-  *head=new_bg_children;
+    new_bg_children->pid=pid_a_ajouter;
+    new_bg_children->next=*head;
+    *head=new_bg_children;
 
 }
 
 void print_bg(bg_children_t *head){
-  bg_children_t * tmp=head;
-  while(tmp!=NULL){
-    printf("%d\n", tmp->pid);
-    tmp=tmp->next;
-  }
+    bg_children_t * tmp=head;
+    while(tmp!=NULL){
+        printf("%d\n", tmp->pid);
+        tmp=tmp->next;
+    }
 }
 
 void remove_bg(bg_children_t **head, pid_t pid){
-  bg_children_t *tmp=*head;
-  bg_children_t *tmp2=*head;
-  if (tmp->pid==pid)
-    *head=tmp->next;
-  else {
-    tmp2=tmp->next;
-    while (tmp2!=NULL && tmp2->pid!=pid){
-      tmp=tmp2;
-      tmp2=tmp2->next;
-      }
-   }
-  if (tmp2!=NULL){
-    tmp->next=tmp2->next;
-  free(tmp2);
-  }
+    bg_children_t *tmp=*head;
+    bg_children_t *tmp2=*head;
+    if (tmp->pid==pid)
+        *head=tmp->next;
+    else {
+        tmp2=tmp->next;
+        while (tmp2!=NULL && tmp2->pid!=pid){
+            tmp=tmp2;
+            tmp2=tmp2->next;
+        }
+    }
+    if (tmp2!=NULL){
+        tmp->next=tmp2->next;
+        free(tmp2);
+    }
 }
 
 void update_bg(bg_children_t **head){
     bg_children_t * tmp=*head;
-  
-  while(tmp!=NULL){
-    int status;
-    pid_t child = waitpid(tmp->pid, &status, WNOHANG);
-    if(child == 0){
-      O//n ne fait rien l'enfant est encore en vie \n
-    }else{
-      remove_bg(&liste_children_bg, tmp->pid);
-    }
-    tmp=tmp->next;
 
-  }
+    while(tmp!=NULL){
+        int status;
+        pid_t child = waitpid(tmp->pid, &status, WNOHANG);
+        if(child == 0){
+            //n ne fait rien l'enfant est encore en vie \n
+        }else{
+            remove_bg(&liste_children_bg, tmp->pid);
+        }
+        tmp=tmp->next;
+
+    }
 }
-  
+
 
 int executer(char *line)
 {
-  /* Insert your code to execute the command line
-   * identically to the standard execution scheme:
-   * parsecmd, then fork+execvp, for a single command.
-   * pipe and i/o redirection are not required.
-   */
-  struct cmdline *l = parsecmd(& line);
-  pid_t child  = fork();
-  if(child == 0) //c'est le fils
+    /* Insert your code to execute the command line
+     * identically to the standard execution scheme:
+     * parsecmd, then fork+execvp, for a single command.
+     * pipe and i/o redirection are not required.
+     */
+    struct cmdline *l = parsecmd(& line);
+    pid_t child1, child2;
+    if(l->seq[0] != NULL && l->seq[1] == NULL)
     {
-      execvp(l->seq[0][0], l->seq[0]);
-    } 
-  //il s'agit du père
-  int return_status;
-  if(l->bg){
-    add_bg(&liste_children_bg,child);
-  }else{
-  waitpid(child, &return_status, 0);
-  //printf("Not implemented: can not execute %s\n", line);
- }
-/* Remove this line when using parsecmd as it will free it */
-free(line);
-	
-return 0;
+        child1 = fork();
+        if(child1 == 0) //c'est le fils
+        {
+            if(l->in != NULL)
+            {
+                int fd = open(l->in, O_RDONLY);
+                dup2(fd, 0);
+                close(fd);
+            }
+            if(l->out != NULL)
+            {
+                mode_t mode = S_IRUSR | S_IWUSR | S_IRGRP | S_IROTH;
+                int fd = open(l->out, O_RDWR|O_APPEND|O_CREAT, mode);
+                dup2(fd, 1);
+                close(fd);
+            }
+            execvp(l->seq[0][0], l->seq[0]);
+        } 
+        
+    } else if(l->seq[0] != NULL && l->seq[1] != NULL)
+    {
+        int tube[2];
+        pipe(tube);
+        if((child1=fork()) == 0)
+        {
+            //if(l->out != NULL)
+            //{
+            //    mode_t mode = S_IRUSR | S_IWUSR | S_IRGRP | S_IROTH;
+            //    int fd = open(l->out, O_RDWR|O_APPEND|O_CREAT, mode);
+            //    dup2(fd, 1);
+            //    close(fd);
+            //}
+            dup2(tube[0], 0);
+            close(tube[1]);
+            close(tube[0]);
+            execvp(l->seq[1][0], l->seq[1]);
+        }
+        if((child2=fork()) == 0)
+        {
+            //if(l->in != NULL)
+            //{
+            //    int fd = open(l->in, O_RDONLY);
+            //    dup2(fd, 0);
+            //    close(fd);
+            //}
+            dup2(tube[1], 1);
+            close(tube[1]);
+            close(tube[0]);
+            execvp(l->seq[0][0], l->seq[0]);
+        }
+    }
+    //il s'agit du père
+    int return_status;
+    if(l->bg){
+        add_bg(&liste_children_bg,child1);
+        add_bg(&liste_children_bg,child2);
+    }else{
+        if(l->seq[1] == NULL)
+            waitpid(child1, &return_status, 0);
+        else
+            waitpid(child1, &return_status, 0);
+    }
+    /* Remove this line when using parsecmd as it will free it */
+    free(line);
+
+    return 0;
 }
 
 SCM executer_wrapper(SCM x)
 {
-        return scm_from_int(executer(scm_to_locale_stringn(x, 0)));
+    return scm_from_int(executer(scm_to_locale_stringn(x, 0)));
 }
 #endif
 
 
 void terminate(char *line) {
 #ifdef USE_GNU_READLINE
-	/* rl_clear_history() does not exist yet in centOS 6 */
-	clear_history();
+    /* rl_clear_history() does not exist yet in centOS 6 */
+    clear_history();
 #endif
-	if (line)
-	  free(line);
-	printf("exit\n");
-	exit(0);
+    if (line)
+        free(line);
+    printf("exit\n");
+    exit(0);
 }
 
 
 int main() {
-        printf("Variante %d: %s\n", VARIANTE, VARIANTE_STRING);
+    printf("Variante %d: %s\n", VARIANTE, VARIANTE_STRING);
 
 #ifdef USE_GUILE
-        scm_init_guile();
-        /* register "executer" function in scheme */
-        scm_c_define_gsubr("executer", 1, 0, 0, executer_wrapper);
+    scm_init_guile();
+    /* register "executer" function in scheme */
+    scm_c_define_gsubr("executer", 1, 0, 0, executer_wrapper);
 #endif
 
-	while (1) {
-		//struct cmdline *l;
-		char *line=0;
-		//int i, j;
-		char *prompt = "ensishell>";
+    while (1) {
+        //struct cmdline *l;
+        char *line=0;
+        //int i, j;
+        char *prompt = "ensishell>";
 
-		/* Readline use some internal memory structure that
-		   can not be cleaned at the end of the program. Thus
-		   one memory leak per command seems unavoidable yet */
-		line = readline(prompt);
-		if (line == 0 || ! strncmp(line,"exit", 4)) {
-			terminate(line);
-		}
+        /* Readline use some internal memory structure that
+           can not be cleaned at the end of the program. Thus
+           one memory leak per command seems unavoidable yet */
+        line = readline(prompt);
+        if (line == 0 || ! strncmp(line,"exit", 4)) {
+            terminate(line);
+        }
 
-		if(!strncmp(line,"jobs",4)) {
-		  print_bg(liste_children_bg);
-		  }
+        if(!strncmp(line,"jobs",4)) {
+            print_bg(liste_children_bg);
+        }
 
 #ifdef USE_GNU_READLINE
-		add_history(line);
+        add_history(line);
 #endif
 
 
 #ifdef USE_GUILE
-		/* The line is a scheme command */
-		if (line[0] == '(') {
-			char catchligne[strlen(line) + 256];
-			sprintf(catchligne, "(catch #t (lambda () %s) (lambda (key . parameters) (display \"mauvaise expression/bug en scheme\n\")))", line);
-			scm_eval_string(scm_from_locale_string(catchligne));
-			free(line);
-                        continue;
-                }
+        /* The line is a scheme command */
+        if (line[0] == '(') {
+            char catchligne[strlen(line) + 256];
+            sprintf(catchligne, "(catch #t (lambda () %s) (lambda (key . parameters) (display \"mauvaise expression/bug en scheme\n\")))", line);
+            scm_eval_string(scm_from_locale_string(catchligne));
+            free(line);
+            continue;
+        }
 #endif
-      update_bg(&liste_children_bg);
-      executer(line);
-      
-		/* parsecmd free line and set it up to 0 */
-		//l = parsecmd( & line);
+        update_bg(&liste_children_bg);
+        executer(line);
 
-		///* If input stream closed, normal termination */
-		//if (!l) {
-		//  
-		//	terminate(0);
-		//}
-		//
+        /* parsecmd free line and set it up to 0 */
+        //l = parsecmd( & line);
 
-		//
-		//if (l->err) {
-		//	/* Syntax error, read another command */
-		//	printf("error: %s\n", l->err);
-		//	continue;
-		//}
+        ///* If input stream closed, normal termination */
+        //if (!l) {
 
-		//if (l->in) printf("in: %s\n", l->in);
-		//if (l->out) printf("out: %s\n", l->out);
-		//if (l->bg) printf("background (&)\n");
+        //    terminate(0);
+        //}
 
-		///* Display each command of the pipe */
-		//for (i=0; l->seq[i]!=0; i++) {
-		//	char **cmd = l->seq[i];
-		//	printf("seq[%d]: ", i);
-      //                  for (j=0; cmd[j]!=0; j++) {
-      //                          printf("'%s' ", cmd[j]);
-      //                  }
-		//	printf("\n");
-		//}
-	}
+
+
+        //if (l->err) {
+        //    /* Syntax error, read another command */
+        //    printf("error: %s\n", l->err);
+        //    continue;
+        //}
+
+        //if (l->in) printf("in: %s\n", l->in);
+        //if (l->out) printf("out: %s\n", l->out);
+        //if (l->bg) printf("background (&)\n");
+
+        ///* Display each command of the pipe */
+        //for (i=0; l->seq[i]!=0; i++) {
+        //    char **cmd = l->seq[i];
+        //    printf("seq[%d]: ", i);
+        //    for (j=0; cmd[j]!=0; j++) {
+        //        printf("'%s' ", cmd[j]);
+        //    }
+        //    printf("\n");
+        //}
+    }
 
 }
